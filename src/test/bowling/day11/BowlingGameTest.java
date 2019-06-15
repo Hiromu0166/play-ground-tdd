5995f3cea2c79ed9c33823e3489201de4f73f831
TadokoroHiromu <hiromu1996166IT@gmail.com>
2019-06-15 13:57:32 +0900

    Bowling Game Kata: Day11(Time: 15.5min->15.0min)

---
+++ src/test/bowling/day11/BowlingGameTest.java
@@ -0,0 +1,66 @@
+package bowling.day11;
+
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BowlingGameTest {
+
+	private Game g;
+
+	@BeforeEach
+	private void setUp() {
+		g = new Game();
+	}
+	private void rollMany(int n, int pins) {
+		for(int i = 0; i < n; i++) {
+			g.roll(pins);
+		}
+	}
+
+	@Test
+	private void rollStrike() {
+		g.roll(10);
+	}
+	private void rollSpare() {
+		g.roll(5);
+		g.roll(5);
+	}
+
+	void testGutterGame() {
+		rollMany(20, 0);
+		assertEquals(0, g.score());
+	}
+
+
+	@Test
+	void testAllOneGame() {
+		rollMany(20, 1);
+		assertEquals(20, g.score());
+	}
+
+	@Test
+	void testOneSpare() {
+		rollSpare();
+		g.roll(3);
+		rollMany(17, 0);
+		assertEquals(16, g.score());
+	}
+
+	@Test
+	void testOneStrike() {
+		rollStrike();
+		g.roll(3);
+		g.roll(4);
+		rollMany(16, 0);
+		assertEquals(24, g.score());
+	}
+
+	@Test
+	void testPerfectGame() {
+		rollMany(12, 10);
+		assertEquals(300, g.score());
+	}
+
+}
